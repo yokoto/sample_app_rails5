c046342034d426634fdc44fa417c481ad0ce5d61
yokoto <youkotoushi@gmail.com>
2019-04-14 17:36:55 +0900

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
@@ -65,6 +66,10 @@ class User < ApplicationRecord
     # パスワード再設定メールの送信時刻が、現在時刻より2時間以上前(早い)の場合
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
+  end
+
+  def feed
+    Micropost.where("user_id = ?", id)
   end
 
   private
